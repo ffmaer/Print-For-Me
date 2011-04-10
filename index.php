ae619582d03d37d84ccf6772109b1df01ef252cc
wyattisimo <jared@wyatt.co>
2011-04-10 02:55:21 -0400

    .

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
 	
 	function printButtonDown() {
 		$('#print_button').attr('src', 'images/print_down.png');
-		//alert('Vivek is stinky.');
 	}
 	
 	function printButtonUp() {
@@ -26,10 +25,45 @@
 
 <body>
 	<div id="container">
-		<form name="print_form" id="print_form" method="post" action="">
-			<input type="file" name="file_to_print" id="file_to_print" /><br />
-			<input type="image" id="print_button" src="images/print_up.png" onmousedown="printButtonDown();" onmouseup="printButtonUp();" />
+		
+		<form name="print_form" id="print_form" method="post" action="putRequest.php" enctype="multipart/form-data">
+
+			<label>price:</label>
+			<input type="text" name="price"/>
+			<br />
+
+			<label>place:</label>
+			<input type="text" name="place"/>
+			<br />
+
+			<label>time:</label>
+			<input type="text" name="time"/>
+			<br />
+
+			<label>file:</label>
+			<input type="file" name="file" id="file" />
+			<br />
+
+			<input type="image" id="print_button" src="images/print_up.png" onmousedown="printButtonDown();" onmouseup="printButtonUp();" onmouseout="printButtonUp();" />
 		</form>
+		
+		<form id="signup_form" action="signUp.php" method="post" enctype="multipart/form-data">
+
+			<label>username:</label>
+			<input type="text" name="username"/>
+			<br />
+
+			<label>password1:</label>
+			<input type="text" name="password1"/>
+			<br />
+
+			<label>password2:</label>
+			<input type="text" name="password2"/>
+			<br />
+
+			<input type="submit" value = "sign up"/>
+		</form>
+		
 	</div>
 </body>
 
