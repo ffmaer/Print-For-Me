d3d1acd1677c5d33d4976b4eceee457ad986b886
ffmaer <ffmaer@gmail.com>
2011-04-10 01:08:33 -0400

    able to write and read files

--- putRequest.php
+++ putRequest.php
@@ -3,8 +3,34 @@
 
 $user_id = $POST['user_id'];
 $price = $_POST['price'];
-$place_id = $_POST['place_id'];
-$time = $_POST['time'];
+$venue_id = $_POST['place_id'];
+$unix_time = $_POST['time'];
+$file_name = $_FILES['file']['name']; 
 
-//how to upload a file?
+$m = new Mongo();
 
+$db = $m->prints;
+$grid = $db->getGridFS();
+$tmp_name = $_FILES['file']['tmp_name'];
+$file_id = $grid->storeFile($tmp_name); 
+
+
+$obj = array(	 "user_id"		=>	$user_id,
+				 "price"		=>	$price,
+				 "venue_id"		=>	$venue_id,
+				 "unix_time"	=>	$unix_time,
+				 "file_name"	=>	$file_name,
+				 "file_id"		=>	$file_id);
+
+$collection = $m->prints->buyers;
+$collection->insert($obj);
+
+
+
+// Stream image to browser
+
+/*
+$image = $grid->findOne(array('name' =>$filename));
+header('Content-type: image/png');
+echo $image->getBytes();
+*/
