375bb59d8702fc7cfcf7cfd5f2e523dbd4440ce8
tcz <ffmaer@gmail.com>
2020-11-12 10:19:18 +0800

    Update signUp.php

--- signUp.php
+++ signUp.php
@@ -1,11 +1,115 @@
 <?php
-require_once("config.php");
+session_start();
+?>
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+<!doctype html>
+<html lang="en">
 
-$obj = array(	 "username"		=>	$username,
-				 "password"		=>	$password );
+<head>
+	<title>printpuppy</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css" />
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.5.2.js"></script>
+	<script type="text/javascript" src="resources/jquery/jquery.validate.js"></script>
+	<script type="text/javascript" src="resources/jquery/jquery.form.js"></script>
+	<script type="text/javascript" src="resources/jquery/jquery.simplemodal-1.4.1.js"></script>
+	
+	<script type="text/javascript">
+	
+	function ajaxResponseHandler(response, status)
+	{
+		$('#response').html(response.message)
+		$('#response').modal({
+			modal: false,
+			escClose: true
+		});
+		if (response.status_code == 200)
+			setTimeout('window.location = "index.php";', 2000);
+		else
+			setTimeout('$("#response").css("display", "none");', 2000);
+	}
+	
+	$(document).ready(function() {
+		
+		var validator = $('#signup_form').validate({
 
-$collection = $m->prints->users;
-$collection->insert($obj);
+			// submit action
+			submitHandler: function(form) {
+				$(form).ajaxSubmit({
+					dataType: 'json',
+					success: ajaxResponseHandler
+				});
+				return false;
+			},
+			
+			// error messages
+			errorPlacement: function(error, element) {
+				error.appendTo(element.prev());
+			},
+
+			// rules
+			rules: {
+				email: {
+					required: true,
+					email: true
+				},
+				password1: {
+					required: true
+				},
+				password2: {
+					required: true,
+					equalTo: '#password1'
+				}
+			},
+			
+			// error messages
+			messages: {
+				email: {
+					required: 'Email is required!',
+					email: 'Invalid email address!'
+				},
+				password1: {
+					required: 'Password is required!'
+				},
+				password2: {
+					required: 'Password is required!',
+					equalTo: 'Passwords must match!'
+				}
+			}
+		});
+			
+	});
+	
+	</script>
+</head>
+
+<body>
+	<div id="response"></div>
+	
+	<div id="container">
+		
+		<h1>Sign Up</h1>
+		
+		<form name="signup_form" id="signup_form" method="post" action="signupRequest.php">
+
+			<label>email address:</label>
+			<input type="text" name="email" id="email" />
+			<br />
+
+			<label>password:</label>
+			<input type="text" name="password1" id="password1" />
+			<br />
+			
+			<label>re-enter password:</label>
+			<input type="text" name="password2" id="password2" />
+			<br />
+			
+			<input type="submit" value="submit" />
+		</form>
+		
+		<hr />
+		
+		<a href="index.php">Print</a>
+	</div>
+</body>
+
+</html>
